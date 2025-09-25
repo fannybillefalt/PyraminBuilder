3ffd6f01420c3e27a8eda34d88c098107b0a1065
fannybillefalt <fannybillefalt@gmail.com>
2025-09-25 08:44:27 +0200

    Created a pyramid builder with nested for-loops

--- PyraminBuilder/Program.cs
+++ PyraminBuilder/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Vilken höjd?");
+            int height = int.Parse(Console.ReadLine());
+
+            for (int rows = 1; rows <= height; rows++)
+            { 
+                for (int spaces = 1; spaces <= height - rows; spaces ++)
+                {
+                    Console.Write(" ");
+                
+                }
+                    for (int stars = 1; stars <= 2 * rows -1 ; stars++)
+
+                    {
+                        Console.Write("*");
+                    }
+
+                Console.WriteLine();
+
+            }
+
+
+
         }
     }
 }
